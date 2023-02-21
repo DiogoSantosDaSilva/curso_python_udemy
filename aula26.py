3c09e8e930b7461fbd1c667772d7b053eeb8c263
Diogo Santos Da Silva <99877866+DiogoSantosDaSilva@users.noreply.github.com>
2023-02-20 23:34:50 -0300

    Aula e exercício
    Na aula 26 se falou sobre fatiamento de strings, no ex26, se usou os conceitos da aula 26 e conceitos anteriores á aula 26.

---
+++ aula26.py
@@ -0,0 +1,17 @@
+"""
+Fatiamento de strings
+012345678
+Olá mundo
+-987654321
+Fatiamento [i:f:p] [::]
+obs.: a função len retorna a qunatidade
+de caracteres da str
+"""
+
+variavel = 'Olá mundo'
+print(variavel[::-1])
+
+
+
+
+
