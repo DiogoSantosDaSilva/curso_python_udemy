c3cde2b6f680b73351e0ce46808059ea2a56fee5
Diogo Santos Da Silva <99877866+DiogoSantosDaSilva@users.noreply.github.com>
2023-02-23 23:05:56 -0300

    Continuação de Variavel constante
    Aula 29 exemplo de variavel constante.
    Aula 30 a identidade de uma variavel na mémoria.

---
+++ aula30.py
@@ -0,0 +1,19 @@
+"""
+Flag (Bandeira) - Marcar um local
+None = Não valor
+is e is not = é ou não é (tipo, valor, identidade)
+id = Identidade 
+"""
+v1 = 'a'
+v2 = 'b'
+print(id(v1))
+print(id(v2))
+
+
+
+# condicao = False
+
+# if condicao:
+#.....print('Faça algo')
+# else:
+#.....print('não faça algo')
